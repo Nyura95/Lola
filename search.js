4f7665513ee933bcb22486c22a50f0282f253d09
Nyura95 <taxisoopya@hotmail.fr>
2018-08-14 11:38:44 +0200

    update

--- search.js
+++ search.js
@@ -1,61 +1,101 @@
+// default values
 var _keys = ['title'];
-var _template = '<div>%title%</div>';
+var _template = '<div>{{title}}</div>';
+
+// private variables
 var _saveItems;
+var _search;
 
-function Search(id, input, items) {
-  this.id = id;
+function Search(main, input, items, options = {}) {
+  this.main = main;
   this.input = input;
-  this.items = items;
-  _saveItems = items;
 
-  if (!Array.isArray(this.items)) {
+  if (options.template) {
+    _template = options.template;
+  }
+
+  if (options.keys) {
+    _keys = options.keys;
+  }
+
+  if (!Array.isArray(items)) {
     throw 'this element must be a array';
   }
 
-  var _self = this;
-  this.getInputSearch().onkeyup = function(e) {
-    _self.searchFunction(e.target.value);
-    _self.reset();
-    _self.writeItems();
+  // rewrite get/set
+  var _items = items;
+  Object.defineProperty(this, 'items', {
+    get: function() {
+      return _items;
+    },
+    set: function(value) {
+      if (_items.length != value.length) {
+        _items = value;
+        _writeItems(value);
+      }
+    }
+  });
+
+  // rewrite push function
+  this.items.push = function(data) {
+    Array.prototype.push.apply(this, data);
+    _writeItems(this);
   };
 
-  this.writeItems();
+  // access to the class privately
+  _search = this;
+
+  // save items
+  _saveItems = items;
+
+  // event keyup input
+  this.input.onkeyup = function(e) {
+    _searchFunction(e.target.value);
+  };
+
+  // start
+  _writeItems(items);
 }
 
-Search.prototype.writeItems = function() {
-  for (let i = 0; i < this.items.length; i++) {
-    this.createNewItem(this.items[i]);
+Search.prototype.getItems = function() {
+  return _saveItems;
+};
+
+// private function
+function _writeItems(items) {
+  _reset();
+  for (let i = 0; i < items.length; i++) {
+    _createNewItem(items[i]);
   }
-};
+}
 
-Search.prototype.getMain = function() {
-  return document.getElementById(this.id);
-};
-
-Search.prototype.getInputSearch = function() {
-  return document.getElementById(this.input);
-};
-
-Search.prototype.reset = function() {
-  this.getMain().innerHTML = '';
-};
-
-Search.prototype.createNewItem = function(item) {
+function _createNewItem(item) {
   var template = _template;
   for (let i = 0; i < _keys.length; i++) {
-    template = template.replace('%' + _keys[i] + '%', item[_keys[i]]);
+    template = template.replace('{{' + _keys[i] + '}}', item[_keys[i]]);
   }
-  this.getMain().innerHTML = this.getMain().innerHTML + template;
-};
+  _search.main.innerHTML = _search.main.innerHTML + template;
+}
 
-Search.prototype.searchFunction = function(value) {
-  this.items = [];
+function _reset() {
+  // remove all html in the main div
+  _search.main.innerHTML = '';
+}
+
+function _searchFunction(value) {
+  // reset
+  _search.items = [];
+
+  // search items
+  var items = [];
   for (let i = 0; i < _saveItems.length; i++) {
     for (let y = 0; y < _saveItems[i].tags.length; y++) {
       if (_saveItems[i].tags[y].indexOf(value) !== -1) {
-        this.items.push(_saveItems[i]);
+        items.push(_saveItems[i]);
         break;
       }
     }
   }
-};
+  // draw all items finded
+  _search.items = items;
+}
